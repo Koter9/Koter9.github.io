3df8c853b248e379603f3890f93f23aedd66a6c6
Koter9 <35015295+Koter9@users.noreply.github.com>
2018-01-10 20:28:31 +0300

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,32 @@
+
+
+function post2 () {
+
+document.getElementById('2p').className +="demo-card-wide mdl-card mdl-shadow--2dp";
+document.getElementById('2p').style.display='block';
+document.getElementById('2t').className +="mdl-card__supporting-text";
+document.getElementById('2t').innerHTML = 'да' ;
+document.getElementById('22').className +="mdl-card__actions mdl-card--border";
+document.getElementById('2b').className +="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect";
+document.getElementById('2b').setAttribute('href', '#2');
+document.getElementById('2b').innerHTML = 'нажми' ;
+document.getElementById('2').className +="mdl-card__title";
+document.getElementById('2').setAttribute('src', 'images/тут мог быть ваш заказ.png');
+document.getElementById('a2').className +="window";
+document.getElementById('wp2').className +="windowImg";
+document.getElementById('wp2').setAttribute('src', 'images/тут мог быть ваш заказ.png');
+document.getElementById('close2').className +="fa fa-times fa-lg";
+document.getElementById('close2').setAttribute('aria-hidden', 'true');
+document.getElementById('ob2').className +="windowOrderButtons";
+document.getElementById('f2').className +="windowOrderButtonTelegram";
+document.getElementById('f2').setAttribute('target', '_blank');
+document.getElementById('f2').setAttribute('href', 'https://freelancehunt.com/freelancer/Koter9.html');
+document.getElementById('f2').innerHTML = 'Freelancehunt' ;
+document.getElementById('t2').className +="windowOrderButtonFrelanchat";
+document.getElementById('t2').setAttribute('target', '_blank');
+document.getElementById('t2').setAttribute('href', 'https://t.me/Koter9');
+document.getElementById('t2').innerHTML = 'Telegram' ;
+document.getElementById('wt2').className +="windowText";
+document.getElementById('wt2').innerHTML = '' ;
+
+}
